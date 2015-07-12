af47541a4d7dbd1915e2b7895c6362a26b0ca5e3
Luke Melia <luke@lukemelia.com>
2015-07-12 01:39:41 -0400

    Restructure to use ember-cli-deploy-plugin base class

--- index.js
+++ index.js
@@ -6,83 +6,45 @@ var fs        = require('fs');
 var path      = require('path');
 var minimatch = require('minimatch');
 
-var chalk     = require('chalk');
-var blue      = chalk.blue;
-var red       = chalk.red;
-
-var validateConfig = require('./lib/utilities/validate-config');
+var DeployPluginBase = require('ember-cli-deploy-plugin');
 
 module.exports = {
   name: 'ember-cli-deploy-manifest',
 
   createDeployPlugin: function(options) {
-    function _generateManifest(distDir, distFiles, filePattern, manifestPath, ui) {
-      var filesToInclude = distFiles.filter(minimatch.filter(filePattern, { matchBase: true }));
-      filesToInclude.sort();
-      var outputPath = path.join(distDir, manifestPath);
-      fs.writeFileSync(outputPath, filesToInclude.join('\n'));
-      _successMessage(ui, filesToInclude.length);
-    }
-
-    function _beginMessage(ui, manifestPath) {
-      ui.write(blue('|      '));
-      ui.write(blue('- generating manifest at `' + manifestPath + '`\n'));
-
-      return Promise.resolve();
-    }
-
-    function _successMessage(ui, count) {
-      ui.write(blue('|      '));
-      ui.write(blue('- generated manifest including ' + count + ' files ok\n'));
-    }
-
-    function _errorMessage(ui, error) {
-      ui.write(blue('|      '));
-      ui.write(red('- ' + error + '`\n'));
-
-      return Promise.reject(error);
-    }
-
-    function _resolveConfigValue(key, config, context) {
-      if (typeof config[key] === 'function') {
-        return config[key](context);
-      }
-
-      return config[key];
-    }
-
-    return {
+    var DeployPlugin = DeployPluginBase.extend({
       name: options.name,
-
-      configure: function(context) {
-        var deployment = context.deployment;
-        var ui         = deployment.ui;
-        var config     = deployment.config[this.name] = deployment.config[this.name] || {};
-
-        return validateConfig(ui, config)
-          .then(function() {
-            ui.write(blue('|    '));
-            ui.writeLine(blue('- config ok'));
-          });
+      defaultConfig: {
+        filePattern: '**/*.{js,css,png,gif,jpg,map,xml,txt,svg,eot,ttf,woff,woff2}',
+        manifestPath: 'manifest.txt',
+        distDir: function(context) {
+          return context.distDir;
+        },
+        distFiles: function(context) {
+          return context.distFiles || [];
+        }
       },
 
-      willUpload: function(context) {
-        var deployment = context.deployment;
-        var ui         = deployment.ui;
-        var config     = deployment.config[this.name] || {};
+      willUpload: function(/* context */) {
+        var filePattern  = this.readConfig('filePattern');
+        var distDir      = this.readConfig('distDir');
+        var distFiles    = this.readConfig('distFiles');
+        var manifestPath = this.readConfig('manifestPath');
 
-        var filePattern  = _resolveConfigValue('filePattern', config, context);
-        var distDir      = _resolveConfigValue('distDir', config, context);
-        var distFiles    = _resolveConfigValue('distFiles', config, context);
-        var manifestPath = _resolveConfigValue('manifestPath', config, context);
-
-        return _beginMessage(ui, manifestPath)
-          .then(function() {
-            _generateManifest.call(this, distDir, distFiles, filePattern, manifestPath, ui);
-            return { manifestPath: manifestPath };
-          })
-          .catch(_errorMessage.bind(this, ui));
+        this.log('generating manifest at `' + manifestPath + '`');
+        try {
+          var filesToInclude = distFiles.filter(minimatch.filter(filePattern, { matchBase: true }));
+          filesToInclude.sort();
+          var outputPath = path.join(distDir, manifestPath);
+          fs.writeFileSync(outputPath, filesToInclude.join('\n'));
+          this.log('generated manifest including ' + filesToInclude.length + ' files ok');
+          return { manifestPath: manifestPath };
+        } catch (error) {
+          this.log(error, { color: 'red' });
+          return Promise.reject(error);
+        }
       }
-    };
+  });
+    return new DeployPlugin();
   }
 };
--- tests/unit/index-nodetest.js
+++ tests/unit/index-nodetest.js
@@ -7,10 +7,17 @@ var path  = require('path');
 var rimraf  = Promise.denodeify(require('rimraf'));
 
 describe('manifest plugin', function() {
-  var subject;
+  var subject, mockUi;
 
-  before(function() {
+  beforeEach(function() {
     subject = require('../../index');
+    mockUi = {
+      messages: [],
+      write: function() { },
+      writeLine: function(message) {
+        this.messages.push(message);
+      }
+    };
   });
 
   it('has a name', function() {
@@ -37,18 +44,51 @@ describe('manifest plugin', function() {
       });
 
       var context = {
-        deployment: {
-          ui: { write: function() {}, writeLine: function() {} },
-          config: {
-            manifest: {
-              filePattern: '**/*',
-              manifestPath: 'manifest.txt'
-            }
+        ui: mockUi,
+        config: {
+          manifest: {
+            filePattern: '**/*',
+            manifestPath: 'manifest.txt'
           }
         }
       };
 
-      return assert.isFulfilled(plugin.configure.call(plugin, context))
+      plugin.beforeHook(context);
+      plugin.configure(context);
+      assert.ok(true); // it didn't throw
+    });
+    describe('without providing config', function () {
+      var plugin, context, config;
+      beforeEach(function() {
+        config = { };
+        plugin = subject.createDeployPlugin({
+          name: 'manifest'
+        });
+        context = {
+          ui: mockUi,
+          config: config
+        };
+        plugin.beforeHook(context);
+      });
+      it('warns about missing optional config', function() {
+        plugin.configure(context);
+        var messages = mockUi.messages.reduce(function(previous, current) {
+          if (/- Missing config:\s.*, using default:\s/.test(current)) {
+            previous.push(current);
+          }
+
+          return previous;
+        }, []);
+        assert.equal(messages.length, 4);
+      });
+
+      it('adds default config to the config object', function() {
+        plugin.configure(context);
+        assert.isDefined(config.manifest.filePattern);
+        assert.isDefined(config.manifest.manifestPath);
+        assert.isDefined(config.manifest.distDir);
+        assert.isDefined(config.manifest.distFiles);
+      });
     });
   });
 
@@ -68,16 +108,14 @@ describe('manifest plugin', function() {
           'assets/bar-da3d0fb7db52f8273550c11403df178f.css',
           'index.html',
         ],
-        deployment: {
-          ui: { write: function() {} },
-          project: { name: function() { return 'test-project'; } },
-          config: {
-            manifest: {
-              filePattern: '**/*.{js,css,png,gif,jpg,map,xml,txt,svg,eot,ttf,woff,woff2}',
-              manifestPath: 'manifest.txt',
-              distDir: function(context){ return context.distDir; },
-              distFiles: function(context){ return context.distFiles; }
-            }
+        ui: mockUi,
+        project: { name: function() { return 'test-project'; } },
+        config: {
+          manifest: {
+            filePattern: '**/*.{js,css,png,gif,jpg,map,xml,txt,svg,eot,ttf,woff,woff2}',
+            manifestPath: 'manifest.txt',
+            distDir: function(context){ return context.distDir; },
+            distFiles: function(context){ return context.distFiles; }
           }
         }
       };
@@ -87,6 +125,7 @@ describe('manifest plugin', function() {
       fs.writeFileSync(path.join(context.distDir, context.distFiles[0]), 'alert("Hello foo world!");', 'utf8');
       fs.writeFileSync(path.join(context.distDir, context.distFiles[1]), 'body { color: red; }', 'utf8');
       fs.writeFileSync(path.join(context.distDir, context.distFiles[2]), '<html>Hello world</html>', 'utf8');
+      plugin.beforeHook(context);
     });
 
     afterEach(function(){
@@ -94,20 +133,20 @@ describe('manifest plugin', function() {
     });
 
     it('includes the matching files in a manifest', function(done) {
-      return assert.isFulfilled(plugin.willUpload.call(plugin, context))
-        .then(function(result) {
-          var manifestContents = fs.readFileSync(path.join(context.distDir, 'manifest.txt'), { encoding: 'utf8' });
-          var lines = manifestContents.split("\n");
-          assert.equal(lines.length, 2);
-          assert.equal(lines[0], "assets/bar-da3d0fb7db52f8273550c11403df178f.css");
-          assert.equal(lines[1], "assets/foo-178d195608c0b18cf0ec5e982b39cad8.js");
-          assert.deepEqual(result, { manifestPath: 'manifest.txt' });
-          done();
-        }).catch(function(reason){
-          console.log(reason);
-          console.log(reason.actual.stack);
-          done(reason);
-        });
+      var result = plugin.willUpload(context);
+      try {
+        var manifestContents = fs.readFileSync(path.join(context.distDir, 'manifest.txt'), { encoding: 'utf8' });
+        var lines = manifestContents.split("\n");
+        assert.equal(lines.length, 2);
+        assert.equal(lines[0], "assets/bar-da3d0fb7db52f8273550c11403df178f.css");
+        assert.equal(lines[1], "assets/foo-178d195608c0b18cf0ec5e982b39cad8.js");
+        assert.deepEqual(result, { manifestPath: 'manifest.txt' });
+        done();
+      } catch(err) {
+        console.log(err);
+        console.log(err.stack);
+        done(err);
+      }
     });
   });
 });
